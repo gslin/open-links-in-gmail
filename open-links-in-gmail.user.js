b8b7479517d28ea3d794994912376a30f2031924
Gea-Suan Lin <gslin@gslin.org>
2019-05-15 13:30:07 +0800

    Import.

---
+++ open-links-in-gmail.user.js
@@ -0,0 +1,42 @@
+// ==UserScript==
+// @name         Open Links in Gmail
+// @namespace    https://wiki.gslin.org/wiki/Open_Links_in_Gmail
+// @version      0.20190515.0
+// @description  Open all matching links in Gmail using "i".
+// @author       Gea-Suan Lin <darkkiller@gmail.com>
+// @match        https://mail.google.com/*
+// @grant        GM_addStyle
+// @grant        GM_getValue
+// @grant        GM_openInTab
+// @grant        GM_registerMenuCommand
+// @grant        GM_setValue
+// @require      https://raw.githubusercontent.com/odyniec/MonkeyConfig/master/monkeyconfig.js
+// @license      MIT
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    let cfg = new MonkeyConfig({
+        menuCommand: true,
+        params: {
+            match_regex: {
+                type: 'text',
+                default: '^https://github\.com/[^/]+/[^/]+/commit/',
+            },
+        },
+        title: 'Open Links in Gmail',
+    });
+
+    let match_regex = new RegExp(cfg.get('match_regex'));
+    window.addEventListener('keydown', ev => {
+        if ('i' === ev.key) {
+            for (let el of document.querySelectorAll('div[role="listitem"]:first-child a')) {
+                let href = el.getAttribute('href');
+                if (href.match(match_regex)) {
+                    GM_openInTab(href, true);
+                }
+            }
+        }
+    });
+})();
